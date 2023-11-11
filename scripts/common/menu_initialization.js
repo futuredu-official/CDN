295c110b52531407e4422ecea062f50b86ae9326
Jaeseong Yoo <praster1@gmail.com>
2023-11-11 22:53:33 +0900

    메뉴 수정

---
+++ scripts/common/menu_initialization.js
@@ -0,0 +1,7 @@
+// 메뉴 하위메뉴 모두 닫기
+function menu_initialization() {
+	document.getElementById("mega_menu_dropdown_guide").className = "hidden"
+	toggle_menu_dropdown_guide = false
+	document.getElementById("mega_menu_dropdown_intro").className = "hidden"
+	toggle_menu_dropdown_intro = false
+}
