226b1a000da8a64d79cc6d7a4c33e7f8c86e2af8
Jaeseong Yoo <praster1@gmail.com>
2024-02-05 06:19:13 +0900

    Div_header

---
+++ scripts/license/Div_header.js
@@ -0,0 +1,8 @@
+function Div_header() {
+	return (
+		<h2 class="text-4xl font-extrabold">
+			<span class="underline underline-offset-3 decoration-8 decoration-blue-400">서류평가 시스템</span>
+			<span class="ml-4 text-xl font-normal">라이센스 키 확인</span>
+		</h2>
+	)
+}
