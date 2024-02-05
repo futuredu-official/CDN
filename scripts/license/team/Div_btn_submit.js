445890336178b9eb47fed49aa96b492f6f3d7cfd
Jaeseong Yoo <praster1@gmail.com>
2024-02-05 23:27:28 +0900

    텍스트 변경

--- scripts/license/team/Div_btn_submit.js
+++ scripts/license/team/Div_btn_submit.js
@@ -4,7 +4,7 @@ function Div_btn_submit() {
 				class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2
 					hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 "
 				onClick={() => get_auth_code()}>
-			라이센스 키 확인
+			기관회원 신청
 		</button>
 	)
 }
