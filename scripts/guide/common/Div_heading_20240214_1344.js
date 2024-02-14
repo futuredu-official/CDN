1b073bf5c5eb66af025a5a2ab610de657e04fe45
Jaeseong Yoo <praster1@gmail.com>
2024-02-14 13:45:06 +0900

    Div_heading_20240214_1344

---
+++ scripts/guide/common/Div_heading_20240214_1344.js
@@ -0,0 +1,11 @@
+function Div_heading(props) {
+	return (
+		<h2 class="flex flex-row justify-start items-center mb-2 text-md font-semibold text-gray-900 px-2 py-4 rounded-lg 
+				   border-2 border-gray-200 shadow cursor-pointer
+				   hover:bg-blue-100"
+			onClick={props.function}>
+			<img src="https://cdn.jsdelivr.net/gh/futuredu-official/CDN/images/svg/heading.svg" class="flex-shrink-0 mr-2 size-5" />
+			{props.title}
+		</h2>
+	)
+}
---
+++ scripts/guide/common/init_variables.js
@@ -0,0 +1,1 @@
+let class_slide = "flex flex-col space-y-2 py-4 w-full"
